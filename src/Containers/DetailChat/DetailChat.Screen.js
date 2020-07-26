7f1d5fe795b332c67fe8c451494e2fe961de3eaa
Apple <ngovanson196@gmail.com>
2020-07-26 21:33:45 +0700

    mock detail chat

--- src/Containers/DetailChat/DetailChat.Screen.js
+++ src/Containers/DetailChat/DetailChat.Screen.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect, useCallback} from 'react';
 import {
   StatusBar,
   Text,
@@ -14,10 +14,31 @@ import {useNavigation} from '@react-navigation/native';
 import {TouchableRipple} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import AppStyles from '../../styles';
+import {GiftedChat, Send} from 'react-native-gifted-chat';
 
 const DetailChatScreen = () => {
+  const [messages, setMessages] = useState([]);
   const navigation = useNavigation();
   const onPress = () => {};
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ]);
+  }, []);
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    );
+  }, []);
 
   const renderToolbar = () => {
     return (
@@ -61,14 +82,45 @@ const DetailChatScreen = () => {
     );
   };
 
+  const renderAction = () => {
+    return (
+      <TouchableRipple
+        onPress={onPress}
+        style={styles.icon}
+        rippleColor="rgba(0, 0, 0, .32)">
+        <Icon size={24} color={AppStyles.colors.accentColor} name="image" />
+      </TouchableRipple>
+    );
+  };
+
+  const renderSend = () => {
+    return (
+      <TouchableRipple
+        onPress={onPress}
+        style={styles.icon}
+        rippleColor="rgba(0, 0, 0, .32)">
+        <Icon size={24} color={AppStyles.colors.accentColor} name="image" />
+      </TouchableRipple>
+    );
+  };
+
   return (
     <Fragment>
       <SafeAreaView style={{flex: 1}}>
         <View style={styles.mainContainer}>
           {renderToolbar()}
-          <Text style={styles.textContent}>
-            Example this is the detail chat screen
-          </Text>
+          <GiftedChat
+            messages={messages}
+            onSend={messages => onSend(messages)}
+            user={{
+              _id: 1,
+            }}
+            placeholder={'Type a message...'}
+            alwaysShowSend={true}
+            isKeyboardInternallyHandled={false}
+            // renderActions={renderAction}
+            // renderSend={renderSend}
+          />
         </View>
       </SafeAreaView>
     </Fragment>
