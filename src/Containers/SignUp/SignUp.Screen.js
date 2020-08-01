6d8f28a5134981f2a4d6f319694c15b58bfa33c4
Apple <ngovanson196@gmail.com>
2020-08-02 00:08:00 +0700

    ui signup

--- src/Containers/SignUp/SignUp.Screen.js
+++ src/Containers/SignUp/SignUp.Screen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -12,11 +12,19 @@ import {
 import * as Animatable from 'react-native-animatable';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Feather from 'react-native-vector-icons/Feather';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import styles from './SignUp.Style';
 
 const SignUpScreen = ({navigation}) => {
+  const [show, setShow] = React.useState(false);
   const [data, setData] = React.useState({
-    username: '',
+    first_name: '',
+    last_name: '',
+    phone: '',
+    email: '',
     password: '',
+    gender: '',
+    birth: '',
     confirm_password: '',
     check_textInputChange: false,
     secureTextEntry: true,
@@ -67,6 +75,15 @@ const SignUpScreen = ({navigation}) => {
     });
   };
 
+  const handleConfirmDate = date => {
+    console.log(date);
+    setShow(false);
+  };
+
+  const hideDatePicker = () => {
+    setShow(false);
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor="#009387" barStyle="light-content" />
@@ -75,11 +92,11 @@ const SignUpScreen = ({navigation}) => {
       </View>
       <Animatable.View animation="fadeInUpBig" style={styles.footer}>
         <ScrollView>
-          <Text style={styles.text_footer}>Username</Text>
+          <Text style={styles.text_footer}>FirstName</Text>
           <View style={styles.action}>
             <FontAwesome name="user-o" color="#05375a" size={20} />
             <TextInput
-              placeholder="Your Username"
+              placeholder="Your FirstName"
               style={styles.textInput}
               autoCapitalize="none"
               onChangeText={val => textInputChange(val)}
@@ -90,7 +107,93 @@ const SignUpScreen = ({navigation}) => {
               </Animatable.View>
             ) : null}
           </View>
+          <Text style={[styles.text_footer, {marginTop: 15}]}>LastName</Text>
+          <View style={styles.action}>
+            <FontAwesome name="user-o" color="#05375a" size={20} />
+            <TextInput
+              placeholder="Your LastName"
+              style={styles.textInput}
+              autoCapitalize="none"
+              onChangeText={val => textInputChange(val)}
+            />
+            {data.check_textInputChange ? (
+              <Animatable.View animation="bounceIn">
+                <Feather name="check-circle" color="green" size={20} />
+              </Animatable.View>
+            ) : null}
+          </View>
+          <Text style={[styles.text_footer, {marginTop: 15}]}>Email</Text>
+          <View style={styles.action}>
+            <FontAwesome name="envelope-o" color="#05375a" size={20} />
+            <TextInput
+              placeholder="Your Email"
+              style={styles.textInput}
+              autoCapitalize="none"
+              onChangeText={val => textInputChange(val)}
+            />
+            {data.check_textInputChange ? (
+              <Animatable.View animation="bounceIn">
+                <Feather name="check-circle" color="green" size={20} />
+              </Animatable.View>
+            ) : null}
+          </View>
+          <Text style={[styles.text_footer, {marginTop: 15}]}>Phone</Text>
+          <View style={styles.action}>
+            <FontAwesome name="mobile-phone" color="#05375a" size={20} />
+            <TextInput
+              placeholder="Your Phone"
+              style={styles.textInput}
+              autoCapitalize="none"
+              onChangeText={val => textInputChange(val)}
+            />
+            {data.check_textInputChange ? (
+              <Animatable.View animation="bounceIn">
+                <Feather name="check-circle" color="green" size={20} />
+              </Animatable.View>
+            ) : null}
+          </View>
+          <Text style={[styles.text_footer, {marginTop: 15}]}>Birth</Text>
+          <View style={styles.action}>
+            <FontAwesome name="calendar" color="#05375a" size={20} />
+            <TextInput
+              placeholder="Your Birth"
+              style={styles.textInput}
+              autoCapitalize="none"
+              onTouchStart={() => setShow(true)}
+              editable={false}
+            />
 
+            {data.check_textInputChange ? (
+              <Animatable.View animation="bounceIn">
+                <Feather name="check-circle" color="green" size={20} />
+              </Animatable.View>
+            ) : null}
+          </View>
+          <DateTimePickerModal
+            isVisible={show}
+            mode="date"
+            onConfirm={handleConfirmDate}
+            onCancel={hideDatePicker}
+          />
+          <Text style={[styles.text_footer, {marginTop: 15}]}>Gender</Text>
+          <View style={styles.action}>
+            <FontAwesome name="transgender" color="#05375a" size={20} />
+            <View
+              style={styles.view_gender}>
+              <TouchableOpacity>
+                <Text style={styles.element_gender}>Male</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text style={styles.element_gender}>Female</Text>
+              </TouchableOpacity>
+            </View>
+
+            {data.check_textInputChange ? (
+              <Animatable.View animation="bounceIn">
+                <Feather name="check-circle" color="green" size={20} />
+              </Animatable.View>
+            ) : null}
+          </View>
           <Text
             style={[
               styles.text_footer,
@@ -209,69 +312,3 @@ const SignUpScreen = ({navigation}) => {
 };
 
 export default SignUpScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#009387',
-  },
-  header: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    paddingHorizontal: 20,
-    paddingBottom: 10,
-  },
-  footer: {
-    flex: Platform.OS === 'ios' ? 3 : 5,
-    backgroundColor: '#fff',
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
-    paddingHorizontal: 20,
-    paddingVertical: 30,
-  },
-  text_header: {
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  text_footer: {
-    color: '#05375a',
-    fontSize: 18,
-  },
-  action: {
-    flexDirection: 'row',
-    marginTop: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#f2f2f2',
-    paddingBottom: 5,
-  },
-  textInput: {
-    flex: 1,
-    marginTop: Platform.OS === 'ios' ? 0 : -12,
-    paddingLeft: 10,
-    color: '#05375a',
-  },
-  button: {
-    alignItems: 'center',
-    marginTop: 20,
-  },
-  signIn: {
-    width: '100%',
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 10,
-  },
-  textSign: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  textPrivate: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    marginTop: 10,
-  },
-  color_textPrivate: {
-    color: 'grey',
-  },
-});
