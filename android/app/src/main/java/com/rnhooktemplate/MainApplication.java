4c1c650469429f3a0af3f7202e710abded4f1ddb
son2412 <ngovanson196@gmail.com>
2020-11-09 09:06:32 +0700

    fix image crop picker android

--- android/app/src/main/java/com/rnhooktemplate/MainApplication.java
+++ android/app/src/main/java/com/rnhooktemplate/MainApplication.java
@@ -2,6 +2,7 @@ package com.rnhooktemplate;
 
 import android.app.Application;
 import com.facebook.react.ReactApplication;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+                    new PickerPackage(),
                     new ImagePickerPackage(),
                     new ReanimatedPackage(),
                     new VectorIconsPackage(),
